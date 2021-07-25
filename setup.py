755d81ff7a881a72788e57f0eacae868a1010de0
Gagandeep Singh <gagandeepsi@vmware.com>
2021-07-24 19:53:11 -0700

    pushed latest changes

--- setup.py
+++ setup.py
@@ -1,27 +1,46 @@
-from setuptools import setup
-setup(
-    name='elina_nn_domains',
-    version='0.1.0',    
-    description='ELINA Python package for neural network analysis',
-    url='https://github.com/shuds13/pyexample',
-    author='Stephen Hudson',
-    author_email='shudson@anl.gov',
-    license='BSD 2-clause',
-    packages=['pyexample'],
-    install_requires=['mpi4py>=2.0',
-                      'numpy',                     
-                      ],
+import subprocess
+from setuptools import setup, Extension
+from distutils.command.install import install as _install
+from setuptools.command.build_ext import build_ext
 
+class Build(build_ext):
+     """Customized setuptools build command - builds protos on build."""
+     def run(self):
+         protoc_command = ["make"]
+         if subprocess.call(protoc_command) != 0:
+             sys.exit(-1)
+         build_ext.run(self)
+
+
+class install(_install):
+    def run(self):
+        subprocess.call(['make', 'clean', '-C', '.'])
+        subprocess.call(['make', '-C', '.'])
+        _install.run(self)
+        
+setup(
+    name='ELINA_NN_Domains',
+    version='0.0.1',    
+    description='ELINA Python package for neural network certification',
+    url='https://github.com/GgnDpSngh/ELINA_NN_Domains',
+    author='Gagandeep Singh',
+    author_email='ggnds@illinois.edu',
+    license='GNU GPL3',
+    packages=['ELINA_NN_Domains'],
+    
     classifiers=[
         'Development Status :: 1 - Planning',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',  
+        'Intended Audience :: Science/Research/Engineering/Technology',
+        'License :: OSI Approved :: GNU GPL3',  
         'Operating System :: POSIX :: Linux',        
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
+    package_dir={"python_interface": ""},
+    #packages=setuptools.find_packages(where="python_interface"),
+    python_requires=">=3.6",
+    cmdclass={'build_ext': Build,},
+    package_data={'': ['libelinaux.so', 'libelinalinearize.so', 'libelinazonotope.so', 'libzonoml.so', 'libfpppoly.so']},
+    setup_requires = ['setuptools>=18.0', 'Cython'],
+    has_ext_modules=lambda: True,
 )
 
